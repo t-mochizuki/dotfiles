a906007e8d87b309beeaf63c416d5586f593e281
t-mochizuki <119Mochizuki@gmail.com>
2015-01-20 16:49:06 +0900

    Remove  the configuration of the browser
    Often, to become a segmentation fault.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -137,7 +137,7 @@
 (require 'ensime)
 (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
 
-(setq browse-url-browser-function 'w3m-browse-url)
-(autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
-;; optional keyboard short-cut
-(global-set-key "\C-xm" 'browse-url-at-point)
+; (setq browse-url-browser-function 'w3m-browse-url)
+; (autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
+; ;; optional keyboard short-cut
+; (global-set-key "\C-xm" 'browse-url-at-point)
