987681848614ef45868eb928605ef087cb92a10c
Takamasa Mochizuki <t-mochizuki@users.noreply.github.com>
2021-06-10 21:30:15 +0900

    fix a bit

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,49 @@
 #!/bin/zsh
 
+if [[ $SHELL != '/bin/zsh' ]]; then
+	echo 'change shell to zsh'
+	chsh -s /bin/zsh
+fi
+
+if [[ ! -d ~/.nvm ]]; then
+	echo 'install nvm'
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | zsh
+fi
+
+if [[ ! -d ~/.jenv ]]; then
+	echo 'install jenv'
+	git clone https://github.com/jenv/jenv.git ~/.jenv
+	# download jdk from https://adoptopenjdk.net/
+fi
+
+if [[ ! -d ~/tmp ]]; then
+	mkdir -p ~/tmp
+fi
+
+if [[ ! -f ~/tmp/sbt-1.5.3/sbt-1.5.3.tgz ]]; then
+	echo 'download sbt'
+	curl -fLo ~/tmp/sbt-1.5.3/sbt-1.5.3.tgz --create-dirs \
+https://github.com/sbt/sbt/releases/download/v1.5.3/sbt-1.5.3.tgz
+fi
+
+if [[ -f ~/tmp/sbt-1.5.3/sbt-1.5.3.tgz ]]; then
+	cd ~/tmp/sbt-1.5.3
+	tar -xf ~/tmp/sbt-1.5.3/sbt-1.5.3.tgz
+	cd -
+fi
+
+if [[ ! -d ~/sbt-1.5.3 ]]; then
+	echo 'put sbt in my home directory'
+	cp -r ~/tmp/sbt-1.5.3/sbt/ ~/sbt-1.5.3/
+fi
+
+if [[ ! -d ~/.tfenv ]]; then
+	echo 'install tfenv'
+	git clone https://github.com/tfutils/tfenv.git ~/.tfenv
+fi
+
 if [[ ! -f ~/.gitconfig ]]; then
+	echo 'put .gitconfig in my home directory'
 	cp gitconfig ~/.gitconfig
 fi
 
@@ -9,25 +52,35 @@ if [[ ! -d ~/.config/git/ ]]; then
 fi
 
 if [[ ! -f ~/.config/git/ignore ]]; then
+	echo 'put .config/git/ignore in my home directory'
 	cp config/git/ignore ~/.config/git/ignore
 fi
 
 if [[ ! -f ~/.zshenv ]]; then
+	echo 'put .zshenv in my home directory'
 	cp zshenv ~/.zshenv
 fi
 
 if [[ ! -f ~/.zshrc ]]; then
+	echo 'put .zshrc in my home directory'
 	cp zshrc ~/.zshrc
 fi
 
+if [[ ! -d ~/.vim/autoload/ ]]; then
+	mkdir -p ~/.vim/autoload/
+fi
+
+if [[ ! -f ~/.vim/autoload/plug.vim ]]; then
+	echo 'install plug.vim'
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
 if [[ ! -f ~/.vimrc ]]; then
+	echo 'put .vimrc in my home directory'
 	cp vimrc ~/.vimrc
 fi
 
 if [[ ! -d ~/.vim/userautoload/ ]]; then
 	cp -r vim/userautoload/ ~/.vim/userautoload/
-fi
-
-if [[ ! -f ~/.tmux.conf ]]; then
-	cp tmux.conf ~/.tmux.conf
 fi
--- zshenv
+++ zshenv
@@ -1,21 +1,32 @@
 export EDITOR=vim
 
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
+if [[ -d ~/.pyenv ]]; then
+	export PYENV_ROOT="$HOME/.pyenv"
+	export PATH="$PYENV_ROOT/bin:$PATH"
+	eval "$(pyenv init -)"
+fi
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
-# [ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"
+if [[ -d ~/.nvm ]]; then
+	export NVM_DIR="$HOME/.nvm"
+	[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
+	# [ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"
+fi
 
-export JENV_ROOT="$HOME/.jenv"
-export PATH="$JENV_ROOT/bin:$PATH"
-eval "$(jenv init -)"
+if [[ -d ~/.jenv ]]; then
+	export JENV_ROOT="$HOME/.jenv"
+	export PATH="$JENV_ROOT/bin:$PATH"
+	eval "$(jenv init -)"
+fi
+
+if [[ -d ~/.tfenv ]]; then
+	export TFENV_ROOT="$HOME/.tfenv"
+	export PATH="$TFENV_ROOT/bin:$PATH"
+fi
 
 export JAVA_OPTS="-Xmx2G -Xss1M"
 
 alias vi="$HOME/vim-8.2.1512/bin/vim"
 
-alias sbt="$HOME/sbt-1.3.13/bin/sbt"
+alias sbt="$HOME/sbt-1.5.3/bin/sbt"
 
 export LESS='-g -M -R -W -z-4 -x4'
